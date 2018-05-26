fe0b9ffca20e8981277972ac70bdc8576f6805fc
Michael Wu <michaelwu@Michaels-MacBook-Pro.local>
2018-05-26 09:56:30 -0700

    tagging added

--- database/models.py
+++ database/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from datetime import date
 # Create your models here.
-
+from taggit.managers import TaggableManager
 class Company(models.Model):
     company_name = models.CharField(("Company Name"),max_length = 200)
     pub_date = models.DateField(("Publish Date"), default=date.today)
@@ -10,5 +10,7 @@ class Company(models.Model):
     company_email = models.EmailField(blank = True)
     company_description = models.TextField(default = "No Description Available")
     company_image_url = models.URLField(("Company Image URL"), default = "http://www.gladessheriff.org/media/images/most%20wanted/No%20image%20available.jpg")
+    company_tags = TaggableManager()
+
     def __str__(self):
         return self.company_name
